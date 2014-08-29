05340bfad170e2fac392455ff541fbfe67938578
Henrik <henrik.renlund@ucr.uu.se>
2014-08-29 11:40:43 +0200

    About to test wether ... can be passed to two different functions

---
+++ R/modComp.r
@@ -0,0 +1,160 @@
+#' @title Compare covariate influence
+#' @description  Compare different subsets of covariates within a model
+#' @param resp the response variable
+#' @param vars character vector containing the names of all variables of interest
+#' @param model name (as character) of the model
+#' @param covars a list containing index vectors on \code{vars}. Each entry corresponds to an analysis.
+#' @param data location of the variables in \code{vars}
+#' @param uni should univariate analyses be performed (if integer, this is used as an index on \code{vars} to determine which univariate analyses)
+#' @param ci if TRUE confidence intervals are included
+#' @param ... arguments passed to \code{model} and \code{transFormat}. (Note that this could potentially be problematic if \code{model} and \code{transFormat} have identical arguments).)
+#' @author Henrik Renlund
+#' @export
+
+modComp <- function(resp, vars, model, covars, data=NULL, uni=TRUE, ci=TRUE, ...){
+   n <- length(vars)
+   if(is.logical(uni)) if(uni) {
+      uni_filt <- 1:n
+   }
+   if(is.numeric(uni)){
+      uni_filt <- uni
+      uni <- TRUE
+   }
+   kol <- length(covars) + if(uni) 1 else 0
+   DF <- if(!is.null(data)) data else .GlobalEnv
+   v_n <- rep(1, n)
+   rnames <- c()
+   for(k in 1:n){
+      tmp <- get(x=vars[k], DF)
+      if(is.numeric(tmp)) {
+         rnames <- c(rnames, vars[k])
+         next}
+      if(is.factor(tmp))    lev <- levels(tmp)
+      if(is.character(tmp)) lev <- sort(unique(tmp))
+      v_n[k] <- length(lev) - 1
+      tmp1 <- gsub(vars[k],"", lev[-1])
+      tmp2a <- paste(vars[k], tmp1[1])
+      tmp2b <- if(v_n[k]>1) paste( paste(rep(" ", nchar(vars[k])),collapse="" ), tmp1[-1])
+      tmp2 <- c(tmp2a, tmp2b)
+      rnames <- c(rnames, tmp2)
+   }
+   v_indx <- c(1,cumsum(v_n)[-n]+1)
+   rad <- sum(v_n)
+   M <- matrix(NA, nrow=rad, ncol=kol)
+   rownames(M) <- rnames
+   colnames(M) <- c(if(uni) "Univariate" else NULL , sprintf("Model %d", 1:length(covars)))
+   for(k in 1:kol){ # k = 1
+      indx <- if(uni & k==1) 1 else if(uni) k-1 else k 
+      if(uni & k==1){
+         for(i in uni_filt){
+            vs <- vars[i]
+            form <- formula(paste0(resp ," ~ ", paste(vs, collapse=" + ")))
+            MOD <- if(is.data.frame(DF)) model(form, data=DF) else model(form)
+            look <- paste0("(", vs, ")", collapse="|")
+            coef_mod <- coef(MOD)
+            coef_indx <- grepl(look, names(coef_mod))
+            COEF <- gsub(" ", "", transFormat(coef_mod[coef_indx], ...))
+            if(ci){
+               ci_mod <- confint(MOD)
+               ci_indx <- grepl(look, rownames(ci_mod))
+               CI <- confint(MOD)[ci_indx,]
+               COEF <- paste(COEF, klister(CI, ...))
+            }
+            filt <- which(vars %in% vs)
+            M[spann(v_indx[filt], v_n[filt]),indx] <- COEF
+         }
+      } else {
+         vs <- vars[ sort(unique(covars[[indx]])) ]
+         form <- formula(paste0(resp ," ~ ", paste(vs, collapse=" + ")))
+         MOD <- if(is.data.frame(DF)) model(form, data=DF) else model(form)
+         look <- paste0("(", vs, ")", collapse="|")
+         coef_mod <- coef(MOD)
+         coef_indx <- grepl(look, names(coef_mod))
+         COEF <- gsub(" ", "", transFormat(coef_mod[coef_indx], ...))
+         if(ci){
+            ci_mod <- confint(MOD)
+            ci_indx <- grepl(look, rownames(ci_mod))
+            CI <- confint(MOD)[ci_indx,]
+            COEF <- paste(COEF, klister(CI, ...))
+         }
+         filt <- which(vars %in% vs)
+         M[spann(v_indx[filt], v_n[filt]),k] <- COEF
+      }
+   }
+   M
+}
+
+#' @title Transform and format###############################################
+#' @description Apply a function to x, then \code{round} or \code{signif} before formatting
+#' @param x the object
+#' @param fun the function
+#' @param signif if non-NULL this is the 'digits' argument for \code{signif}
+#' @param round if non-NULL this is the 'digits' argument for \code{round}
+#' @param skip if TRUE \code{format} is skipped
+#' @param ... arguments passed to \code{format}
+
+transFormat <- function(x, fun=NULL, signif=NULL, round=NULL, skip=FALSE, ...){
+   if(is.null(fun)) fun <- identity
+   if(is.null(y <- tryCatch(fun(x), error=function(e) NULL))){
+      warning("[transFormat] 'fun' not applicable to 'x' and is set to 'identity'")
+   } else {
+      x <- y
+   }
+   if(!is.null(signif) & !is.null(round)) stop("[transFormat] 'signif' and 'round' should not be non-null simultaneous.")
+   if(!is.null(signif)) x <- signif(x, signif)
+   if(!is.null(round)) x <- round(x, round)
+   if(!skip) x <- format(x, ...)
+   x
+}
+
+# - # @title klister  #########################################################
+# - # @description Glue (collapse) the rows of a matrix into a character vector
+# - # @param M matrix to be collapsed
+# - # @param start start of character entry
+# - # @param stopp end of character entry
+# - # @param sep separator of matrix entries when collapsed
+# - # @param ... arguments passed to \code{transFormat}
+
+klister <- function(M, start="(", stopp=")", sep=",", ...){
+   if(is.null(nrow(M))){
+      kli <- paste0(start, paste0(transFormat(M,...), collapse=sep), stopp)
+   } else {
+      kli <- rep(NA,nrow(M))
+      for(i in 1:nrow(M)){
+         kli[i] <- paste0(start, paste0(transFormat(M[i,],...), collapse=sep), stopp)
+      }
+   }
+   gsub(" ","",kli)
+}
+
+# - # @title Spann ###########################################################
+# - # @description From each point in \code{start}, extend by the corresponding point in \code{length}
+# - # @param start integer vector
+# - # @param length integer vector
+
+spann <- function(start, length) {
+   s <- c()
+   for(k in seq_along(start)){
+      s <- c(s, start[k]:(start[k]+length[k]-1))
+   }
+   if(!setequal(s,unique(s))) warning("[modComp\\spann] spann contains multiplicities")
+   s
+}
+
+# tests
+if(FALSE){
+   foo <- function(x=0, ...){
+      x + bar(...) + baz(...)
+   }
+   bar <- function(y=0, ...) if(y==0) 3 else -3
+   baz <- function(z=0, ...) if(z==0) 10 else -10
+   foo()
+   foo(1)
+   foo(y=1)
+   foo(z=1)
+   foo(y=1,z=1)
+   foo(z=1,y=1)
+   foo(z=1,y=1,100)
+   foo(100,1,1)
+   foo(100,1)
+}
---
+++ inst/tests/test_modComp.r
@@ -0,0 +1,85 @@
+context("all tests of 'modComp', 'transFormat' and their helper functions")
+
+test_that("'modComp' works", {
+   DF <- data.frame(x1=c(1,2,3,4),x2=c(3,4,0,1))
+   DF$y <- 2*DF$x1 + DF$x2 + c(0.1, -0.2, 0.05,0.05)
+   expect_equivalent(
+      modComp(resp = "y", 
+              vars = c("x1", "x2"), 
+              model = lm, 
+              covars = list(1:2), 
+              data = DF, 
+              uni = FALSE, 
+              ci = TRUE, 
+              round=2),
+      matrix(c("1.93 (0.72,3.14)", "0.92 (0.07,1.77)"), nrow=2)
+   )
+   expect_equivalent(
+      modComp(resp = "y", 
+              vars = c("x1", "x2"), 
+              model = lm, 
+              covars = list(1:2), 
+              data = DF, 
+              uni = TRUE, 
+              ci = TRUE, 
+              round=3),
+      matrix(c("1.01 (-1.796,3.816)","1.93 (0.725,3.135)","-0.045 (-2.984,2.894)","0.92 (0.068,1.772)"), nrow=2, byrow=TRUE)
+   )
+   expect_equivalent(
+      modComp(resp = "y", 
+              vars = c("x1", "x2"), 
+              model = lm, 
+              covars = list(1,1:2), 
+              data = DF, 
+              uni = FALSE, 
+              ci = FALSE, 
+              signif=1),
+      matrix(c("1","2.0",NA,"0.9"), nrow=2, byrow=TRUE)
+   )
+   expect_equivalent(
+      modComp(resp = "y", 
+              vars = c("x1", "x2"), 
+              model = lm, 
+              covars = list(1:2), 
+              data = DF, 
+              uni = TRUE, 
+              ci = FALSE, 
+              signif=2,
+              fun=exp),
+      matrix(c("2.7","6.9","0.96","2.5"), nrow=2, byrow=TRUE)
+   )
+   rm(DF)
+   DF <- data.frame(
+      x = c(3,1,2,3,2,4,5,6,4,5,3,2,4,1,1,2,3,4,6,7,8,1,1,2,6,4,2,1,1,3,4),
+      y = c(0,1,1,0,1,0,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,1,1,0,1,0,0,1,0,0),
+      z =  rep(letters[1:2], length.out=31)
+      )
+   glm(y~x+z, data=DF, family="binomial")
+   modComp(resp = "y", 
+           vars = c("x", "z"), 
+           model=glm,
+           covars=list(1:2), 
+           data = DF, 
+           uni=FALSE,
+           ci=FALSE,
+           signif=3,
+           fun=identity)
+   
+   # coxph(Surv(x,y)~z, data=DF)
+})
+
+test_that("'transFormat' works", {
+   expect_equal(transFormat(c(-0.1, 4.0001, 3.5001), digits=2),c("-0.1", " 4.0", " 3.5"))
+   expect_equal(transFormat(c(-0.1, 4.0001, 3.5001), exp),c(" 0.9048374", "54.6036101", "33.1187637"))
+   expect_equal(transFormat(c(-0.1, 4.0001, 3.5001), exp, digits=2),c(" 0.9", "54.6", "33.1"))
+})
+
+test_that("'klister' works", {
+   expect_equal(klister(matrix(1:6, nrow=2)), c("(1,3,5)", "(2,4,6)"))
+})
+
+test_that("spann works", {
+   expect_equal(spann(3,4), 3:6)
+   expect_equal(spann(c(-2,7),c(3,2)), c(-2:0,7:8))
+   expect_equal(spann(c(1,3,4,7), c(2,1,2,5)), c(1:5,7:11))
+})
