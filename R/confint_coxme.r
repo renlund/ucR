8dc50d334363b222bfad01b9dcbc8adf154e43dd
Henrik Renlund <henrikrenlund@gmail.com>
2015-01-20 12:57:36 +0100

    (bugfix)

--- R/confint_coxme.r
+++ R/confint_coxme.r
@@ -1,13 +1,13 @@
 #' @title \code{confint} method for class \code{coxme}
-#' @description This is a crappy solution to getting the confidence intervals
+#' @description This is a outstandingly crappy solution to getting the confidence intervals
 #' for the coefficients from a \code{coxme} object. It captures the output from
 #' \code{print(object)}... Oh, well.
 #' @param object an onbject of class \code{coxme}
+#' @param more set to TRUE if you also want the coefficients and p --values.
 #' @author Henrik Renlund
 #' @export
 
-confint.coxme <- function(object){
-   # CRAPPY SOLUTION!
+confint.coxme <- function(object, more=FALSE){
    foo <- capture.output(object)
    a <- which(foo == "Fixed coefficients") + 1
    b <- which(foo == "Random effects") - 2
@@ -17,8 +17,32 @@ confint.coxme <- function(object){
    co <- as.numeric(unlist(lapply(X = l, FUN = "[", i=indx_co))[-1])
    indx_se <- which(l[[1]] == "se(coef)")
    se <- as.numeric(unlist(lapply(X = l, FUN = "[", i=indx_se))[-1])
-   m <- matrix(c(se - 2*se, se + 2*se), ncol=2)
-   colnames(m) <- c("2.5 %", "97.5 %")
-   rownames(m) <- unlist(lapply(X = l, FUN = "[", i=1))[-1]
+   if(more){
+      indx_p <-  which(l[[1]] == "p")
+      ps <- as.numeric(unlist(lapply(X = l, FUN = "[", i=indx_p))[-1])
+      m <- matrix(c(co - 2*se, co + 2*se, co, ps), ncol=4)
+      colnames(m) <- c("2.5 %", "97.5 %", "coef", "p")
+      rownames(m) <- unlist(lapply(X = l, FUN = "[", i=1))[-1]
+   } else {
+      m <- matrix(c(co - 2*se, co + 2*se), ncol=2)
+      colnames(m) <- c("2.5 %", "97.5 %")
+      rownames(m) <- unlist(lapply(X = l, FUN = "[", i=1))[-1]
+   }
    m
 }
+# confint.coxme <- function(object){
+#    # CRAPPY SOLUTION!
+#    foo <- capture.output(object)
+#    a <- which(foo == "Fixed coefficients") + 1
+#    b <- which(foo == "Random effects") - 2
+#    raw <- foo[a:b]
+#    l <- strsplit(x = raw, split = " +")
+#    indx_co <- which(l[[1]] == "coef")
+#    co <- as.numeric(unlist(lapply(X = l, FUN = "[", i=indx_co))[-1])
+#    indx_se <- which(l[[1]] == "se(coef)")
+#    se <- as.numeric(unlist(lapply(X = l, FUN = "[", i=indx_se))[-1])
+#    m <- matrix(c(se - 2*se, se + 2*se), ncol=2)
+#    colnames(m) <- c("2.5 %", "97.5 %")
+#    rownames(m) <- unlist(lapply(X = l, FUN = "[", i=1))[-1]
+#    m
+# }
